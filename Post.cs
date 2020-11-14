e60e0d022dac190206d1f4ea9dbe4d8702061209
Marcos Motta <mottamarcosthiago@gmail.com>
2020-11-14 14:12:58 -0300

    Projeto HttpClient

---
+++ Post.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TesteHttpClient
+{
+    public class Post
+    {
+        public string Login { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Public_Repos { get; set; }
+        public int Followers { get; set; }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace TesteHttpClient
+{
+    class Program
+    {
+
+        static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+
+            Console.Write("Digigte o login do usuario: ");
+            var userName = Console.ReadLine();
+
+            client.DefaultRequestHeaders.Add("User-Agent", "Other");
+            var response = await client.GetStringAsync("https://api.github.com/users/" + userName);
+            var posts = JsonConvert.DeserializeObject<Post>(response);
+
+            Console.WriteLine();
+            Console.WriteLine($"Login: {posts.Login}");
+            Console.WriteLine($"Id: {posts.Id}");
+            Console.WriteLine($"Nome: {posts.Name}");
+            Console.WriteLine($"Repositórios Publicados: {posts.Public_Repos}");
+            Console.WriteLine($"Numero de Seguidores: {posts.Followers}");
+            Console.WriteLine("----------------------------------------------------");
+            Console.WriteLine();
+        }
+    }   
+}
